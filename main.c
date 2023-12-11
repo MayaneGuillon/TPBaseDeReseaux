631b2b1d0b360fbb3256b1dd84c1a48dcb39c869
MayaneGuillon <mayane.guillon@ensea.fr>
2023-12-11 14:49:43 +0100

    1 gettftp

--- main.c
+++ main.c
@@ -5,8 +5,15 @@
 #include <stdlib.h>
 
 int main (int argc, char ** argv){
+    // gettftp
     if (argc!=4){
         printf("Wrong usage : gettftp filename host port\n");
         exit(EXIT_SUCCESS);
     }
+    // alt256 srvtinfo1.ensea.fr 69
+    const char * filename=argv[1];
+    const char * host=argv[2];
+    const char * port=argv[3];
+    printf("gettftp Server : Filename : %s , Host : %s , Port : %s \n", filename, host, port);
+
 }

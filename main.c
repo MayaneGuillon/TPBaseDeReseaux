b23e472c912ae3e7552db4002b297a4fcb8da813
MayaneGuillon <mayane.guillon@ensea.fr>
2023-12-11 14:12:12 +0100

    First test of argc

---
+++ main.c
@@ -0,0 +1,12 @@
+//
+// Created by ranidaou43 on 11/12/23.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char ** argv){
+    if (argc!=4){
+        printf("Wrong usage : gettftp filename host port\n");
+        exit(EXIT_SUCCESS);
+    }
+}
